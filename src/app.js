7d97d9f432e1e613287f4255b10d80a2b4626fb4
vitormalencar <vitor.maia.alencar@gmail.com>
2020-05-02 13:06:16 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex === -1) {
+    return response.status(400).json({ error: "Repository does not exists." });
+  }
+
+  const repository = {
+    id,
+    url,
+    title,
+    techs,
+    likes: repositories[repoIndex].likes,
+  };
+
+  repositories[repoIndex] = repository;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "not found" });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id, likes } = request.params;
+
+  const repo = repositories.find((repo) => repo.id === id);
+
+  if (!repo) {
+    return response.status(400).json({ error: "not found" });
+  }
+  repo.likes++;
+
+  return response.json(repo);
+});
+
+module.exports = app;
